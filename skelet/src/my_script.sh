90321d2d0bff39351b75efc7675cdc8ae78390df
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-02-21 18:04:35 +0200

    Refs #101376; Updated skelet

--- skelet/src/my_script.sh
+++ skelet/src/my_script.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+
 echo "Start getting data"
 
 echo "Getting data for: $1"
@@ -9,7 +11,7 @@ report_location=$2/$(date +"%FT%H%M%S+0000")
 
 mkdir -p $report_location
 
-docker run --rm <my_docker_to_run> $1 $3 > $report_location/<my_garie_plugin>.txt 2>&1
+timeout <timeout> docker run --rm <my_docker_to_run> $1 $3 > $report_location/<my_garie_plugin>.txt 2>&1
 
 echo "Finished getting data for: $1"
 
