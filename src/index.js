b6fc74520225bd3a49fb36a6de85bf38e01ef9f9
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-03-07 15:20:41 +0200

    Refs #100860; Fixed the issue when influxdb is not responding, now we retry to connect to it

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const { plugin_getMeasurement } = require('./plugin')
 const numCPUs = require('os').cpus().length;
 const mapAsync = require('./utils/map-async');
 const utils = require('./utils');
+const sleep = require('sleep-promise');
 
 async function getDataForItem(item){
 
@@ -32,70 +33,92 @@ const getDataForAllUrls = async(options) => {
 }
 
 const init = async(options) => {
-    var settings = {
-        db_name:'test',
-        config:{
-            "cron": "0 */4 * * *",
-            "urls": [
-                {
-                    "url": "https://www.test.com/",
-                },
-                {
-                    "url": "https://www.test.co.uk/",
+    return new Promise(async (resolve, reject) => {
+        try{
+            var settings = {
+                db_name:'test',
+                config:{
+                    "cron": "0 */4 * * *",
+                    "urls": [
+                        {
+                            "url": "https://www.test.com/",
+                        },
+                        {
+                            "url": "https://www.test.co.uk/",
+                        }
+                    ]
                 }
-            ]
-        }
-    }
-    extend(settings, options);
-    const influx_obj = influx.init(settings.db_name)
-    await influx.create_db(influx_obj);
+            }
+            extend(settings, options);
+            const influx_obj = influx.init(settings.db_name)
 
-    var items = [];
-    for (var i = 0; i <= settings.config.urls.length-1; i++){
-        const url_settings = {};
-        url_settings.url = settings.config.urls[i].url;
-        const { plugins } = settings.config.urls[i];
-        if ((plugins !== undefined) && (plugins[options.plugin_name] !== undefined)){
-            extend(url_settings, plugins[options.plugin_name]);
-        }
-        var tmp_item = {
-            url_settings: url_settings,
-            influx_obj: influx_obj,
-            getData: settings.getData,
-            getMeasurement: settings.getMeasurement,
-            plugin_name: settings.plugin_name,
-            report_folder_name: settings.report_folder_name,
-            app_root: settings.app_root
-        }
-        items.push(tmp_item)
-    }
+            var retries = 0;
+            var shouldInterrupt = false;
+            while(true){
+                try{
+                    console.log('Trying to connect to influx');
+                    await influx.create_db(influx_obj);
+                    console.log('Connected to influx');
+                    break;
+                }
+                catch (err){
+                    retries++;
+                    if (retries < 60){
+                        console.log('Failed to connect to influx, retry #', retries);
+                        await sleep(1000)
+                    }
+                    else {
+                        throw(err);
+                    }
+                }
+            }
 
-    var getAllDataOptions = {};
-    getAllDataOptions.items = items;
-    getAllDataOptions.prepDataForAllUrls = settings.prepDataForAllUrls;
-    try {
-        try {
-            const cron_config = settings.config.plugins[settings.plugin_name].cron
-            if (cron_config) {
-                return new CronJob(
-                    cron_config,
-                    async () => {
-                        getDataForAllUrls(getAllDataOptions);
-                    },
-                    null,
-                    true,
-                    'Europe/London',
-                    null,
-                    true
-                );
+            var items = [];
+            for (var i = 0; i <= settings.config.urls.length-1; i++){
+                const url_settings = {};
+                url_settings.url = settings.config.urls[i].url;
+                const { plugins } = settings.config.urls[i];
+                if ((plugins !== undefined) && (plugins[options.plugin_name] !== undefined)){
+                    extend(url_settings, plugins[options.plugin_name]);
+                }
+                var tmp_item = {
+                    url_settings: url_settings,
+                    influx_obj: influx_obj,
+                    getData: settings.getData,
+                    getMeasurement: settings.getMeasurement,
+                    plugin_name: settings.plugin_name,
+                    report_folder_name: settings.report_folder_name,
+                    app_root: settings.app_root
+                }
+                items.push(tmp_item)
+            }
+
+            var getAllDataOptions = {};
+            getAllDataOptions.items = items;
+            getAllDataOptions.prepDataForAllUrls = settings.prepDataForAllUrls;
+            try {
+                const cron_config = settings.config.plugins[settings.plugin_name].cron
+                if (cron_config) {
+                    return new CronJob(
+                        cron_config,
+                        async () => {
+                            getDataForAllUrls(getAllDataOptions);
+                        },
+                        null,
+                        true,
+                        'Europe/London',
+                        null,
+                        true
+                    );
+                }
+            } catch (err){
+                console.log("Cron is not configured for plugin", err);
             }
         } catch (err){
-            console.log("Cron is not configured for plugin", err);
+            console.log(err);
+            reject(err);
         }
-    } catch (err) {
-        console.log(err);
-    }
-
+    });
 }
 
 module.exports = {
