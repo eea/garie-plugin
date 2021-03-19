506e7077d86a9de238863fabf3c17408c79aa48c
Daniela Mormocea <daniela.mormocea@gmail.com>
2021-03-19 11:49:04 +0200

    prettier timestamp

--- src/index.js
+++ src/index.js
@@ -143,11 +143,11 @@ const getDataForAllUrls = async(options) => {
                 continue;
             }
             if (failedUrls.length === 0){
-                const pointStatusLogsFirst = influx.markStatusLogs(`RETRY ${retries}`, Date.now());
+                //const pointStatusLogsFirst = influx.markStatusLogs(`RETRY ${retries}`, Date.now());
                 console.log("Retry: " + retries + "/" + options.retryTimes);
                 console.log('All tasks were executed successfully');
                 const pointStatusLogsSecond = influx.markStatusLogs("FINISHED", Date.now());
-                await influx.savePoints(options.influx, [pointStatusLogsFirst, pointStatusLogsSecond]);
+                await influx.savePoints(options.influx, [pointStatusLogsSecond], "FINISHED");
                 break;
             }
             else {
